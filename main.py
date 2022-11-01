6e04aa0885ca7da5c5c3c482421dc0bb78074741
somalChakraborty <somal@trademanza.com>
2022-11-01 14:01:05 -0500

    working changes

---
+++ main.py
@@ -0,0 +1,33 @@
+from datetime import date
+from flask import Flask, request, make_response
+from summarizer_processor import summarizeFromContent, summarizeFromURL
+
+app = Flask(__name__)
+
+@app.route('/',methods = ['GET'])
+def hello():
+    return "Testing get";
+
+@app.route('/summarization-from-content',methods = ['GET'])
+def summarizationFromContent():
+    request_data = request.get_json(force=True)
+    content = request_data.get('content')
+
+    summary = summarizeFromContent(content)
+
+    todayDate = date.today()
+
+    response_body = {'summary':summary,'fromContent':True,'fromUrl':False, 'contentDate': todayDate }
+    return response_body, 200, {'Access-Control-Allow-Origin':'*'}
+
+@app.route('/summarization-from-URL',methods = ['GET'])
+def summarizationFromURL():
+    request_data = request.get_json(force=True)
+    url = request_data.get('url')
+
+    summary = summarizeFromURL(url)
+
+    todayDate = date.today()
+
+    response_body = {'summary':summary,'fromContent':False,'fromUrl':True, 'contentDate': todayDate}
+    return response_body, 200, {'Access-Control-Allow-Origin':'*'}
---
+++ summarizer_processor.py
@@ -0,0 +1,35 @@
+import urllib.request 
+import pickle
+from transformers import pipeline
+import bs4 as BeautifulSoup
+
+
+def summarizeFromContent(content):
+    to_tokenize = content
+    summarizer = pipeline("summarization", model="t5-base")
+    summary = summarizer(to_tokenize, min_length=75, max_length=1000)
+    summaryText = summary[0].get("summary_text") if len(summary)>0  else ''
+    return summaryText.capitalize();
+
+
+def summarizeFromURL(url):
+    article_content = fetchDataFromUrl(url)
+    print(article_content[100])
+    summary = summarizeFromContent(article_content)
+    return summary
+
+def fetchDataFromUrl(url):
+    # fetch
+    fetched_data = urllib.request.urlopen(url)
+    article_read = fetched_data.read()
+
+    # parse
+    article_parsed = BeautifulSoup.BeautifulSoup(article_read,'html.parser')
+    paragraphs = article_parsed.find_all('p')
+
+    #populate
+    article_content = ''
+    for p in paragraphs:  
+        article_content += p.text
+    return article_content
+
